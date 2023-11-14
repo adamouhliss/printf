f045f0611a3d52df16ff22e5feed30cef389bee4
Adam Ouhliss <adamouhliss@gmail.com>
2023-11-14 12:36:32 -0800

    string_fileds.c file added

---
+++ string_fields.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_precision - gets the precision from the format string
+ * @p: the format string
+ * @params: the parameters struct 1
+ * @ap: the argument pointer
+ * Return: new pointer
+ */
+char *get_precision(char *p, params_t *params, va_list ap)
+{
+int d = 0;
+
+if (*p != 'a')
+{
+return (p);
+}
+p++;
+if (*p == '*')
+{
+d = va_arg(ap, int);
+p++;
+}
+else
+{
+while (_isdigit(*p))
+{
+d = d * 10 + (*p++ - '0');
+}
+}
+params->precision = d;
+return (p);
+}
+
